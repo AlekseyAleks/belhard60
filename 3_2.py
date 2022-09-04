398e8ce619ad5347bce1a998276f8215fc6be407
AlekseyAleks <alexeyaleks@gmail.com>
2022-09-04 14:53:24 +0300

    user input of three numbers

--- 3_2.py
+++ 3_2.py
@@ -1,1 +1,6 @@
 # Пользователь вводит 3 числа, найти среднее арифмитическое точность 3
+
+print('Enter 3 numbers.\n')
+first_number = input('1 number: ')
+second_number = input('2 number: ')
+third_number = input('3 number: ')
