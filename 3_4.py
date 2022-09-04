b1bb631e729ea10c3cf9ea0b1eea1e2809a6287f
AlekseyAleks <alexeyaleks@gmail.com>
2022-09-04 18:50:56 +0300

    solution tasks 3_1, 3_2, 3_3, 3_4

---
+++ 3_4.py
@@ -0,0 +1,7 @@
+# Пользователь вводит 3 числа, сказать сколько из них положительных и сколько отрицательных
+
+print('Enter 3 numbers.')
+count_negative = input('1 number: ').startswith('-') +\
+                 input('2 number: ').startswith('-') +\
+                 input('2 number: ').startswith('-')
+print(f'Positive numbers: {3 - count_negative} \nNegative numbers: {count_negative}')
