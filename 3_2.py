8b81167de50d0882f8eef2224ccbdbaa3f2450c4
AlekseyAleks <alexeyaleks@gmail.com>
2022-09-04 16:34:59 +0300

    solution task 3_2

--- 3_2.py
+++ 3_2.py
@@ -1,6 +1,8 @@
 # Пользователь вводит 3 числа, найти среднее арифмитическое точность 3
 
-print('Enter 3 numbers.\n')
-first_number = input('1 number: ')
-second_number = input('2 number: ')
-third_number = input('3 number: ')
+print('Enter 3 numbers.')
+first_number = float(input('1 number: '))
+second_number = float(input('2 number: '))
+third_number = float(input('3 number: '))
+arithmetic_average = round((first_number + second_number + third_number) / 3, 3)
+print(f'Arithmetic average: {arithmetic_average}')
