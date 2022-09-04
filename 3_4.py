a2fab15056a4fce255ee984450302643d3a2ad7d
AlekseyAleks <alexeyaleks@gmail.com>
2022-09-04 22:22:00 +0300

    corrections to the printed text to task 3_4

--- 3_4.py
+++ 3_4.py
@@ -3,5 +3,5 @@
 print('Enter 3 numbers.')
 count_negative = input('1 number: ').startswith('-') +\
                  input('2 number: ').startswith('-') +\
-                 input('2 number: ').startswith('-')
+                 input('3 number: ').startswith('-')
 print(f'Positive numbers: {3 - count_negative} \nNegative numbers: {count_negative}')
