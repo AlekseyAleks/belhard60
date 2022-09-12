6e5576219c0356fcfc9dd65968a3bdedb5f24abf
AlekseyAleks <alexeyaleks@gmail.com>
2022-09-12 21:27:38 +0300

    solution task 4_3

--- 4_3.py
+++ 4_3.py
@@ -1,3 +1,10 @@
 # *Заполнить словарь где ключами будут выступать числа от 0 до n, а
 # значениями вложенный словарь с ключами "name" и "email", а значения
 # для этих ключей будут браться с клавиатуры
+
+keys = int(input(f'Enter the number of keys: '))
+my_dict = {}
+for i in range(keys):
+    print(f'Key {i}')
+    my_dict[i] = {'name': input('Enter name: '), 'email': input('Enter email: ')}
+print(my_dict)
