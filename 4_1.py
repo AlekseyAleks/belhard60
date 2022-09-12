697d149167025e424cb7e12b8b530b996cd6142f
AlekseyAleks <alexeyaleks@gmail.com>
2022-09-12 19:23:27 +0300

    update task 4_1, add file 4_3

--- 4_1.py
+++ 4_1.py
@@ -1,7 +1,5 @@
 # Заполнить список степенями числа 2 (от 2^1 до 2^n).
 
 entered_extent = int(input('Введите степень: '))
-my_list = [None] * entered_extent  # [None, None, ..., None]
-for i in range(len(my_list)):
-    my_list[i] = 2 ** (i + 1)
+my_list = [2 ** (i + 1) for i in range(entered_extent)]
 print(my_list)
