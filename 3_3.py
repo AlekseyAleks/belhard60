508edaf7248f59501dc63bc878615789e66bf29e
AlekseyAleks <alexeyaleks@gmail.com>
2022-09-14 12:42:43 +0300

    task 3_3 fix

--- 3_3.py
+++ 3_3.py
@@ -2,9 +2,8 @@
 # сформировать приветственное сообщение путем форматирования 3-мя способами
 
 name = input('Enter name: ')
-age = input('Enter age: ')
+age = int(input('Enter age: '))
 city = input('Enter city: ')
-print('First solution: ', 'Hello ', name, '! ' 'You are ', age, ' years old. ',
-      'You are from the city of ', city, '.', sep='')
+print('First solution:', 'Hello %s! You are %d years old. You are from the city of %s.' % (name, age, city))
 print('Second solution:', 'Hello {}! You are {} years old. You are from the city of {}.'.format(name, age, city))
 print('Third solution:', f'Hello {name}!', f'You are {age} years old.', f'You are from the city of {city}.')
